91f5a759c921e88e36d65e16905349f4ead6ed4f
shahsa6809 <117024958+shahsa6809@users.noreply.github.com>
2022-12-29 16:11:16 +0530

    array

---
+++ optional_chaning.js
@@ -0,0 +1,11 @@
+let obj ={
+    person: {
+        name:'sahil'
+    },
+    arr:[1,2,3,4,5,6],
+    func: () => {
+        console.log('sahil shah');
+    },
+};
+console(obj?.arr?.[2])
+
---
+++ second_array.js
@@ -0,0 +1,5 @@
+const first_array=require('./first_array');
+
+
+const arr=[1,2,3,4,5,6,7];
+console.log(first_array.x);
