2435c21723a35a9feaaf559c4b4eeda0d3f67728
shahsa6809 <117024958+shahsa6809@users.noreply.github.com>
2023-01-03 11:52:26 +0530

    indexjs

---
+++ findIndex.js
@@ -0,0 +1,8 @@
+let users= [
+{id: 1, name: "John"},
+{id: 2, name: "Pete"},
+{id: 3, name: "Mary"}
+
+];
+let user = users.find(item => item.id == 1);
+console.log(user.name);
---
+++ includes_method.js
@@ -0,0 +1,13 @@
+//const arr=[1,2,3,4,5];
+//let getvaluepresentornot=arr.includes(2);
+//console.log(getvaluepresentornot);
+//
+
+//const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+//const citrus = fruits.slice(1, 3);
+//console.log(citrus);
+
+const arr1=[1,2,3];
+const arr2=[2,9,8];
+const arr3=arr1.concat(arr2);
+console.log(arr3);
